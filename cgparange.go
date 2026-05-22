63a4f810710e58bdaf822126b4a2bfd6b67dbb3b
lota <ngenejoshua05@gmail.com>
2026-05-22 10:53:05 +0100

    refactor: fix floating-point gaps and simplify logic in CGPA range

--- cgparange.go
+++ cgparange.go
@@ -1,18 +1,20 @@
 package main
 
+import "math"
+
 func CgpaRange(nb float64) string {
-	if nb < 1 || nb > 5 {
+	if nb < 1.0 || nb > 5.0 || math.IsNaN(nb) {
 		return "Invalid CGPA"
 	}
 
 	switch {
-	case nb >= 4.5 && nb <= 5.00:
+	case nb >= 4.5:
 		return "First class honours"
-	case nb >= 3.50 && nb <= 4.49:
+	case nb >= 3.5:
 		return "Second class honours(Upper Division)"
-	case nb >= 2.40 && nb <= 3.49:
+	case nb >= 2.4:
 		return "Second class honours(Lower Division)"
-	case nb >= 1.50 && nb <= 2.39:
+	case nb >= 1.5:
 		return "Third class honours"
 	default:
 		return "Pass"
